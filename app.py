7324df1b1ec47f85ca4862c00e3b9f97995f0ff6
endodo <kuehnp@bzz.ch>
2025-05-06 16:02:15 +0200

    requirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 from dotenv import load_dotenv
