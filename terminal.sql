b6c2065ce23269961c46b95787f5e1b667f1a5c4
giova <mendolag@bzz.ch>
2025-05-06 11:54:51 +0200

    added taskmaker

--- terminal.sql
+++ terminal.sql
@@ -1,1 +1,14 @@
 use taskplaner;
+select * from aufgabe;
+INSERT INTO Benutzer (BenutzerName, BenutzerPWD)
+VALUES ('testuser', '1234');
+
+CALL CreateTask(
+    'Flask lernen',
+    '2025-05-06 10:00:00',
+    '2025-05-06 12:00:00',
+    'Zuhause',
+    '47.3769, 8.5417',
+    'Basics von Flask durchgehen',
+    1, 1, 1, 1
+);
